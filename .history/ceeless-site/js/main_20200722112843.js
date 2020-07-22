6871de3a12dedb0f6c6968f0e7b93357f172fd54
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2020-07-22 11:44:38 -0400

    added a simple markup

---
+++ .history/ceeless-site/js/main_20200722112843.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });

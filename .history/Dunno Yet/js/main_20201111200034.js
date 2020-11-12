2764ef0a9d9838f419ac59442e59fb3f2bd38bd6
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2020-11-11 21:08:53 -0500

    Added the logo and a basic layout

---
+++ .history/Dunno Yet/js/main_20201111200034.js
@@ -0,0 +1,28 @@
+var position = $(window).scrollTop(); 
+// $(".logo-mobile").hide();
+// $('.logo-web').attr('src', "assets/Logo/ceelesslogoblack2.png").width('170px');
+// should start at 0
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    
+    // if( scroll > position ){
+    //     $('.navbar').addClass('bg-light');
+    //     $(".logo-web").fadeOut('slow');
+    //     $(".logo-mobile").fadeIn('slow');
+    // } else {
+    //     $('.navbar').removeClass('bg-light');
+    //     $(".logo-web").show();
+    //     $(".logo-mobile").hide(); 
+    // }    
+    
+    if( scroll > position ){
+        $('.navbar').addClass('shadow bg-light');
+        // $('.logo-web').attr('src', "assets/Logo/ceeiconblack.png").height('25px').width('auto').addClass('fade-in');
+    } else {
+        $('.navbar').removeClass('shadow bg-light');
+        
+    }    
+    // position = scroll;
+});
+  
---
+++ .history/Dunno Yet/js/main_20201111200418.js
@@ -0,0 +1,29 @@
+var position = $(window).scrollTop(); 
+// $(".logo-mobile").hide();
+// $('.logo-web').attr('src', "assets/Logo/ceelesslogoblack2.png").width('170px');
+// should start at 0
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    
+    // if( scroll > position ){
+    //     $('.navbar').addClass('bg-light');
+    //     $(".logo-web").fadeOut('slow');
+    //     $(".logo-mobile").fadeIn('slow');
+    // } else {
+    //     $('.navbar').removeClass('bg-light');
+    //     $(".logo-web").show();
+    //     $(".logo-mobile").hide(); 
+    // }    
+    
+    if( scroll > position ){
+        $('.navbar').removeClass('bg-transparent')
+        $('.navbar').addClass('shadow bg-white');
+        // $('.logo-web').attr('src', "assets/Logo/ceeiconblack.png").height('25px').width('auto').addClass('fade-in');
+    } else {
+        $('.navbar').removeClass('shadow bg-white');
+        
+    }    
+    // position = scroll;
+});
+  
